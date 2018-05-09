d30194d099dd322ad04f65b1f177a61271a7ec89
Klim Radostev pushina <kradoste@e1z2r3p37.42.us.org>
2018-05-09 15:07:56 -0700

    finished project

---
+++ place_piece.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   place_piece.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: afrantzi <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/04/30 17:42:12 by afrantzi          #+#    #+#             */
+/*   Updated: 2018/05/06 16:33:05 by kradoste         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fillit.h"
+
+void	place_piece(const int *piece, char **board, int pnum)
+{
+	int i;
+	int	cbuf;
+	int rbuf;
+
+	i = 0;
+	cbuf = 0;
+	rbuf = 0;
+	board[g_row][g_col] = 65 + pnum;
+	while (++i < 4)
+	{
+		if (piece[i] > piece[i - 1])
+			cbuf++;
+		else
+		{
+			cbuf = piece[i] - piece[0];
+			rbuf++;
+		}
+		board[g_row + rbuf][g_col + cbuf] = 65 + pnum;
+	}
+	print_board(board);
+	ft_putchar('\n');
+}
